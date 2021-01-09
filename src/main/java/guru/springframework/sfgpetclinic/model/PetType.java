c6aa519c8872d3d8e11a00217bbfc93d3156dd73
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-01-09 11:03:30 +0200

    Basic domain model established

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PetType {
+
+    private String name;
+
+
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Vet extends Person {
+}
