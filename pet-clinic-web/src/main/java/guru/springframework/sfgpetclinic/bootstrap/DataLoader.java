0072eaa48f33258e5ee03acfbb4b2c68c15186f9
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-01-10 13:08:14 +0200

    AbstractMapService now implements logic for autogenerated id key value. Also, the generics have been extended to support this new logic. I slightly disagree with JT's approach here, because this extension is damaging the targeted absolute abstraction of this class. ServiceMap classes refactored accordingly to reflect the removal of the ID argument from the save method constructor. Also, I find JT's logic for providing nextID too complicated, we can use this.map.size() static method to find the next id.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -25,16 +25,16 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 
         int test = 0;
 
+        System.out.println();
+
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner1.setId(2L);
         owner1.setFirstName("Fiona");
         owner1.setLastName("Glenanne");
 
@@ -43,14 +43,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet1.setId(2L);
         vet1.setFirstName("Jessie");
         vet1.setLastName("Porter");
 
