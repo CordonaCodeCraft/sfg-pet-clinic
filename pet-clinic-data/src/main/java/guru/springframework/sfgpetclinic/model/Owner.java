355db998f2f0ac67a7b35e380a5f0704c8836102
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-01-11 20:31:37 +0200

    Updated Owner entity with Set of Pets property. Created Visit entity.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -4,7 +4,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Getter
 @Setter
 public class Owner extends Person {
+
+    private Set<Pet> pets;
 }
