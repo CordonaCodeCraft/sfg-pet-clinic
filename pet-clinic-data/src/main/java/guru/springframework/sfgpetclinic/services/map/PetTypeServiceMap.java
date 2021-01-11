4b8ec72c7e14700cc770a45eed6311d9593589e2
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-01-11 21:34:01 +0200

    Added SpecialtyService and implemented SpecialtyServiceMap.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -1,6 +1,5 @@
 package guru.springframework.sfgpetclinic.services.map;
 
-import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
