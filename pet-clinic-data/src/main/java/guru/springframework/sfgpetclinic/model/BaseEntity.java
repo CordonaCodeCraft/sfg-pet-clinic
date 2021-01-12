265df8b77f9461b65e21d0fc9860069b3a226af6
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-01-12 18:17:39 +0200

    Base entity implemented as a @MappedSuperClass and now has @Id property.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -3,12 +3,20 @@ package guru.springframework.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
+
+import static javax.persistence.GenerationType.IDENTITY;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
 }
